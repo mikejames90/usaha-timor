e21af9b28f8d9bc92ebc9098c3a4d8fed7ca9903
ericktimothy <ericktimothy@gmail.com>
2017-08-31 00:12:15 +1000

    setup initial project

---
+++ src/main/java/com/usti/repository/CustomerRepository.java
@@ -0,0 +1,8 @@
+package com.usti.repository;
+
+import com.usti.entity.Customer;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long>{
+
+}
