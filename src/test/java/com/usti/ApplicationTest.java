b6a367b9cc6e145d4dd4363fbda357a40d40acda
etimothy <etimothy@viator.com>
2017-09-01 13:44:21 +1000

    setup basic layout

--- src/test/java/com/usti/ApplicationTest.java
+++ src/test/java/com/usti/ApplicationTest.java
@@ -25,7 +25,7 @@ public class ApplicationTest {
     @Test
     public void homePage() throws Exception {
         // N.B. jsoup can be useful for asserting HTML content
-        mockMvc.perform(get("/index.html"))
+        mockMvc.perform(get("/"))
                 .andExpect(content().string(containsString("Customer")));
     }
 
