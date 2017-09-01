84996fc347cf5aead3cce1f5ba2094ef07ee2063
etimothy <etimothy@viator.com>
2017-09-01 17:44:35 +1000

    initial listing page

--- src/main/java/com/usti/controller/CustomerController.java
+++ src/main/java/com/usti/controller/CustomerController.java
@@ -1,8 +1,11 @@
 package com.usti.controller;
 
+import com.usti.entity.Customer;
 import com.usti.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -14,7 +17,12 @@ public class CustomerController {
     private CustomerRepository customerRepository;
 
     @RequestMapping(method = RequestMethod.GET)
-    private String customerList() {
+    private String customerList(Model model) {
+        PageRequest pageRequest = new PageRequest(0, 50);
+
+        Iterable<Customer> customerList = customerRepository.findAll(pageRequest);
+
+        model.addAttribute("customerList", customerList);
         return "customer-list";
     }
 }
--- src/main/java/com/usti/repository/CustomerRepository.java
+++ src/main/java/com/usti/repository/CustomerRepository.java
@@ -1,8 +1,8 @@
 package com.usti.repository;
 
 import com.usti.entity.Customer;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface CustomerRepository extends CrudRepository<Customer, Long> {
+public interface CustomerRepository extends PagingAndSortingRepository<Customer, Long> {
 
 }
