5172a4a3865abed94e6e39f5afce3c6e47d1dfd5
ericktimothy <ericktimothy@gmail.com>
2017-09-03 00:43:58 +1000

    init Vue.js

---
+++ src/main/frontend/App.old.vue
@@ -0,0 +1,19 @@
+<template>
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <a class="navbar-brand" href="#">Usaha Timor</a>
+    </nav>
+    <div class="container-fluid">
+        <div class="row">
+            <nav class="col-sm-3 col-md-2 d-none d-sm-block bg-light sidebar">
+                <ul class="nav nav-pills flex-column">
+                    <li class="nav-item">
+                        <router-link to="/customer" class="nav-link">Customer</router-link>
+                    </li>
+                </ul>
+            </nav>
+            <main class="col-sm-9 ml-sm-auto col-md-10 pt-3" role="main">
+                <router-view class="view"></router-view>
+            </main>
+        </div>
+    </div>
+</template>
